76366255e812e7ff49fd144076bd8b7f6c04cfd4
TheForeverOptimist <sean@elevate.app>
2023-03-13 16:41:09 -0400

    user schema

---
+++ models/user.js
@@ -0,0 +1,21 @@
+const mongoose = require('mongoose');
+
+
+//defining user schema
+const userSchema = new mongoose.Schema({
+    username: {
+        type: String,
+        required: true
+    },
+    email: {
+        type:String,
+        required: true
+    },
+    password: {
+        type:String,
+        required: true
+    }
+});
+
+const User = mongoose.model('User', CommentSchema);
+module.exports = User
