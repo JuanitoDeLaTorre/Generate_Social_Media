09b72c7473ca859b93f0766e3c5d02cbb12ffe99
TheForeverOptimist <sean@elevate.app>
2023-03-13 16:42:35 -0400

    created controller, config, env, gitignore

---
+++ controller/controller.js
@@ -0,0 +1,16 @@
+const express = require('express');
+const router = express.Router();
+const User = require('../../models/users');
+const Post = require('../../models/post');
+const Like = require('../../models/likes');
+const Comment = require('../../models/comments');
+
+router.get('/posts/:id', async (req, res) => {
+    try {
+        const post = await Post.findById(req.params.id);
+        res.render('post', { post });
+    } catch (error) {
+        console.error(error);
+        res.status(500).json({error: 'Failed to retrieve post'});
+    }
+});
