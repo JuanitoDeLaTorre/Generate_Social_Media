5b1e9f0b431ac588a70407b6db11108a0e0e63b7
TheForeverOptimist <sean@elevate.app>
2023-03-14 10:46:44 -0400

    profile pic in user schema

--- models/user.js
+++ models/user.js
@@ -14,6 +14,10 @@ const userSchema = new mongoose.Schema({
     password: {
         type:String,
         required: true
+    },
+    profilePic: {
+        type:String,
+        required: true
     }
 });
 
