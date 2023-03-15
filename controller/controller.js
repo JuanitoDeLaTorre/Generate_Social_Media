525e7864d9a6c17d8b155a4f099c2d251d69853b
TheForeverOptimist <sean@elevate.app>
2023-03-15 13:58:01 -0400

    adding stuff to controllers

--- controller/controller.js
+++ controller/controller.js
@@ -5,6 +5,40 @@ const Post = require('../../models/post');
 const Like = require('../../models/likes');
 const Comment = require('../../models/comments');
 
+
+//CREATE SESSION
+app.use(
+    session({
+        store: MongoStore.create({mongoUrl:process.env.GENERATE_DB}),
+        secret: "super secret",
+        resave: false,
+        saveUninitialized: false,
+        cookie: {maxAge: 1000 * 60 * 60 * 24 * 7}
+        
+    })
+)
+
+app.get('/', (req,res)=> {
+    res.render("index.ejs");
+})
+
+app.get('/signUp', (req,res) => {
+    res.render('signUp.ejs');
+})
+
+app.get('/profile', (req,res) => {
+    res.render('profile.ejs');
+})
+app.post('/users/signUp', async (req,res,next)=> {
+    try {
+        console.log(req.body)
+        const newUser = await User.create(req.body)
+        res.redirect('/')
+    } catch(err) {
+        console.log(err)
+        next()
+    }
+})
 //get all users
 router.get('/users', async (req, res) => {
     try {
@@ -33,4 +67,4 @@ router.post('/posts', async (req, res) => {
 })
 
 //update a post
-router.put
+// router.put
